a9e1898f8120ff1bee80579ee500ecdff95f5d00
Alexey M <moiseev27@gmail.com>
2022-11-11 20:17:19 +0530

    Add circle tests

---
+++ ShapesLibrary.Tests/CircleTest.cs
@@ -0,0 +1,24 @@
+using ShapesLibrary.Core;
+
+namespace ShapesLibrary.Tests
+{
+    public class CircleTest
+    {
+        [Theory]
+        [InlineData(-1)]
+        public void InstantiationFailedTest(double radius)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(radius));
+        }
+
+        [Theory]
+        [InlineData(1, 1 * Math.PI)]
+        [InlineData(5, 25 * Math.PI)]
+        [InlineData(10, 100 * Math.PI)]
+        public void GetAreaTest(double radius, double area)
+        {
+            Circle circle = new Circle(radius);
+            Assert.Equal(circle.GetArea(), area);
+        }
+    }
+}

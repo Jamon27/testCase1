8c253c505a85c82ca557fa262ebed3ba3b4225ee
Alexey M <moiseev27@gmail.com>
2022-11-11 21:04:02 +0530

    Implement Facade

---
+++ ShapesLibrary/ShapesFacade.cs
@@ -0,0 +1,26 @@
+﻿using ShapesLibrary.Core.Interface;
+using ShapesLibrary.Core.Shapes;
+
+namespace ShapesLibrary.Core
+{
+    public class ShapesFacade: IShapesFacade
+    {
+        public double GetCircleArea(double radius)
+        {
+            Circle circle = new Circle(radius);
+            return circle.GetArea();
+        }
+
+        public double GetTriangleArea(double abSide, double bcSide, double acSide)
+        {
+            Triangle triangle = new Triangle(abSide, bcSide, acSide);
+            return triangle.GetArea();
+        }
+
+        public bool IsOrthogonalTriangle(double abSide, double bcSide, double acSide)
+        {
+            Triangle triangle = new Triangle(abSide, bcSide, acSide);
+            return triangle.IsOrthogonal();
+        }
+    }
+}
